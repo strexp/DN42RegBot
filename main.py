23923c454f99b60117dbb549878f4a3d2e442b02
Charles <Charles@0x7f.cc>
2021-08-25 14:52:09 +0800

    Fix bug for job queue

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ updater = Updater(token=sys.argv[1], use_context=True)
 
 dispatcher = updater.dispatcher
 
-jobq = updater.job_queue
-
 REGPATH = os.path.expanduser('~') + "/registry"
 
 CACHEPATH = os.path.expanduser('~') + "/cache_data"
@@ -36,15 +34,15 @@ def get_key(dat, key):
             ret = ret + ln
         return ret
 
-def send_new(context, asn):
+def send_new(asn):
     with open(REGPATH + '/' + asn) as asn_file:
         asn_data = asn_file.read().splitlines()
     asn_info = process_file(asn_data)
     new_arr = [get_key(asn_info, k) for k in ['aut-num','as-name','descr','mnt-by','country','org']]
-    context.bot.send_message(chat_id=sys.argv[2], 
+    dispatcher.bot.send_message(chat_id=sys.argv[2], 
                              text=TEMPLATE.format(new_arr))
 
-def main_task(context: CallbackContext):
+def main():
     print("Starting new task...")
     asn_list = [f for f in listdir(REGPATH)]
     asn_new = []
@@ -54,12 +52,8 @@ def main_task(context: CallbackContext):
     with open(CACHEDIR + '/asn.txt', 'w') as asn_file:
         asn_file.write('\n'.join(asn_list))
     for new_asn in asn_new:
-        send_new(context, new_asn)
+        send_new(new_asn)
         time.sleep(3)
-
-def main():
-    main_job = jobq.run_repeating(main_task, interval=60, first=0)
-    print("Bot started.")
 
 if __name__ == '__main__':
     main()
